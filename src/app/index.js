1895d80e717117977a6b7d9d8bddb57d86ad88d7
Hieu Trinh <trieuhieu509a@gmail.com>
2017-05-12 18:38:58 +0700

    Using redux

--- src/app/index.js
+++ src/app/index.js
@@ -1,3 +1,4 @@
+/*
 import React from "react";
 import {render} from "react-dom";
 
@@ -28,4 +29,36 @@ class App extends React.Component {
     }
 }
 
-render(<App />, window.document.getElementById('app'));
+render(<App />, window.document.getElementById('app'));*/
+
+
+import { createStore } from "redux";
+
+const reducer = (state, action) => {
+    switch (action.type){
+        case "ADD":
+            state = state + action.payload;
+            break;
+        case "SUBTRACT":
+            state = state - action.payload;
+            break;
+    }
+
+    return state;
+};
+
+const store = createStore( reducer,1);
+
+store.subscribe(()=>{
+    console.log("Store updated!", store.getState() );
+});
+
+store.dispatch({
+    type: "ADD",
+    payload: 22
+});
+
+store.dispatch({
+    type: "SUBTRACT",
+    payload: 11
+});
